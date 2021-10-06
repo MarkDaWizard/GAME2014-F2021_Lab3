2e2b378d493ca685eec6e727660f175147bcea45
Mark <wushu4@yahoo.com.vn>
2021-10-05 20:57:46 -0400

    Added enemy

---
+++ Assets/[Scripts]/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    [Header ("EnemyMovement")]
+    public Bounds movementBounds;
+    public Bounds startingRange;
+    private float startingPoint;
+    private float randomSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpeed = Random.Range(movementBounds.min, movementBounds.max);
+        startingPoint = Random.Range(startingRange.min, startingRange.max);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(Mathf.PingPong(Time.time, randomSpeed) + startingPoint, transform.position.y);
+    }
+}
