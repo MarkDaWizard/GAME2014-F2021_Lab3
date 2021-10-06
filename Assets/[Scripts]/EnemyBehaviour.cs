d6fff0ce3f4efe9b1a15d76b8d76e2720a9a1372
Mark <wushu4@yahoo.com.vn>
2021-10-05 22:41:25 -0400

    Added bullet manager

--- Assets/[Scripts]/EnemyBehaviour.cs
+++ Assets/[Scripts]/EnemyBehaviour.cs
@@ -10,16 +10,37 @@ public class EnemyBehaviour : MonoBehaviour
     public Bounds startingRange;
     private float startingPoint;
     private float randomSpeed;
+
+    [Header("Bullets")]
+    public Transform bulletSpawn;
+    //public GameObject bulletPrefab;
+
+    private BulletManager bulletManager;
+    public int frameDelay;
+
+
     // Start is called before the first frame update
     void Start()
     {
         randomSpeed = Random.Range(movementBounds.min, movementBounds.max);
         startingPoint = Random.Range(startingRange.min, startingRange.max);
+        bulletManager = GameObject.FindObjectOfType<BulletManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector2(Mathf.PingPong(Time.time, randomSpeed) + startingPoint, transform.position.y);
+    }
+
+    private void FixedUpdate()
+    {
+        if(Time.frameCount % frameDelay == 0)
+        {
+            // var temp_bullet = Instantiate(bulletPrefab);
+            //temp_bullet.transform.position = bulletSpawn.position;
+
+            bulletManager.GetBullet(bulletSpawn.position);
+        }
     }
 }
